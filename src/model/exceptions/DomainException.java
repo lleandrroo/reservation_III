cc4ebd7c7895f7d8da3613eaecb38644cbfafc0d
lleandrroo <lleandrroo@hotmail.com>
2022-09-23 13:37:52 -0300

    Good solution

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,22 @@
+package model.exceptions;
+
+public class DomainException extends Exception {
+	
+	/*
+	 * 	DomainException can extends both by Exception or RuntimeException
+	 * 	
+	 * 	Exception - compilador obriga a tratar exceção
+	 * 	RuntimeException - compilador não obriga a tratar exceção
+	 * 	
+	 * 	The serializable class DomainException does not declare a static final
+	 * 	serialVersionUID field of type lon
+	 * 
+	 * 
+	 * */
+	
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
